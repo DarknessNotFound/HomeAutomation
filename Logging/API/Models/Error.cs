08a08b3d7a8d9b2574bcb7b3639b68f2921a8912
DarknessNotFound <galbrightr@gmail.com>
2022-11-26 21:45:26 -0600

    data model setup

--- Logging/API/Models/Error.cs
+++ Logging/API/Models/Error.cs
@@ -1,4 +1,15 @@
-class Error
+public class Error
 {
+
+    [DbColumn(IsIdentity =true, IsPrimary =true)]
+    int Id { get; set; }
     
+    [DbColumn]
+    string File { get; set;}
+    
+    [DbColumn]
+    string Function { get; set; }
+    
+    [DbColumn]
+    string Message { get; set; }
 }
--- Logging/API/Program.cs
+++ Logging/API/Program.cs
@@ -1,3 +1,5 @@
+using System.Data.SQLite;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
