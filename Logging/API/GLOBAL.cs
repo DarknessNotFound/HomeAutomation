49d846de15fd53d78d925b108f65970e8a86cadb
DarknessNotFound <galbrightr@gmail.com>
2022-12-13 19:48:28 -0600

    readwrite

--- Logging/API/GLOBAL.cs
+++ Logging/API/GLOBAL.cs
@@ -4,6 +4,6 @@ static class Constants
     public static readonly string DB = "Logs.db";
 
     //Connection string to the DB
-    public static readonly string Conn = "Data Source=" + DB + ";";
+    public static readonly string Conn = "Data Source=" + DB + ";Mode=ReadWriteCreate";
     public static readonly string ConnRead = "Data Source=" + DB + ";Mode=ReadOnly";
 }
